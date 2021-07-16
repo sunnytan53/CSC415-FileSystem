76c7d69690dcb50dc64485938aebb015fb0174b5
CiYuan53 <haoyuan.t@outlook.com>
2021-07-15 22:32:15 -0700

    rough code for milestone 1 (works)
    Co-authored-by: ychi1 <ychi1@users.noreply.github.com>
    Co-authored-by: minseon-park <minseon-park@users.noreply.github.com>
    Co-authored-by: Guo-1999 <Guo-1999@users.noreply.github.com>

--- fsInit.c
+++ fsInit.c
@@ -21,32 +21,43 @@
 #include <sys/types.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 #include "fsLow.h"
 #include "mfs.h"
-#include <math.h>
+
+// define constant values for our file system
+#define BIT_SIZE_OF_INT (sizeof(int) * 8)
+#define MAGIC_NUMBER "FIORE_FILESYSTEM"
+#define MAGIC_NUMBER_LENGTH 16 // must manually change if MAGIC_NUMBER changes
+#define SPACE_FREE 0
+#define SPACE_USED 1
+#define TYPE_DIR -1
 
 // define the structure for a vcb
-typedef struct vcb
+typedef struct
 {
-	char magicNumber[16];	 // can be altered later
-	uint64_t numberOfBlocks; // must be this type for LBA function works
+	char magicNumber[MAGIC_NUMBER_LENGTH];
 	uint64_t blockSize;		 // must be this type for LBA function works
-	uint64_t feespaceLength;
-} vcb_t, *vcb_p;
+	uint64_t numberOfBlocks; // must be this type for LBA function works
+	uint freespaceBlockCount;
+	uint rootDirLoc;
+} vcb;
 
 // init related function prototype
-void initVCB(uint64_t, uint64_t);
-void initFreeSpace(uint64_t, uint64_t);
-void initRootDir();
 char *setCleanBuffer(uint64_t);
+void initVCB(uint64_t, uint64_t);
+void initFreeSpace();
+void initRootDir();
+uint createDirectory(dirEntry *, char *);
+uint64_t allocateFreeSpace(uint);
+void updateFreeSpace();
+void setBitOn();
 
-// define global values for the file system
-vcb_p ourVCB;
+// declare global values for the file system
+vcb *ourVCB;
 int *freespace;
-
-// define a constant magic number for our file system
-const char MAGIC_NUMBER[] = "FIORE_FILESYSTEM";
+uint64_t freespaceSize;
 
 char *setCleanBuffer(uint64_t bufferSize)
 {
@@ -64,7 +75,7 @@ int initFileSystem(uint64_t numberOfBlocks, uint64_t blockSize)
 	printf("Initializing File System with %ld blocks with a block size of %ld\n", numberOfBlocks, blockSize);
 
 	// prepare a clean buffer to read the data of the VCB
-	// must preapare because LBARead() reads a whole block
+	// must preapare a whole block because LBARead() reads a whole block
 	char *readBuffer = setCleanBuffer(blockSize);
 
 	// read the FIRST block of our sample volume into the buffer
@@ -72,85 +83,205 @@ int initFileSystem(uint64_t numberOfBlocks, uint64_t blockSize)
 	LBAread(readBuffer, 1, 0);
 
 	// allocate space for our VCB and its pointer
-	ourVCB = malloc(sizeof(vcb_t));
+	ourVCB = malloc(sizeof(vcb));
 
 	// copy the needed amount of data into our VCB
-	memcpy(ourVCB, readBuffer, sizeof(vcb_p));
+	memcpy(ourVCB, readBuffer, sizeof(vcb));
 
 	// free the unused buffer
 	free(readBuffer);
 	readBuffer = NULL;
 
-	// initialize the VCB and other things if it is not initialized
-	// this must be done by checking the magic number of the volume
-	// this ensure the volume is the type of our file system
-	if (memcmp(ourVCB->magicNumber, MAGIC_NUMBER, strlen(MAGIC_NUMBER)) != 0)
+	if (memcmp(ourVCB->magicNumber, MAGIC_NUMBER, MAGIC_NUMBER_LENGTH) == 0)
 	{
-		initVCB(numberOfBlocks, blockSize);
-		initFreeSpace(numberOfBlocks, blockSize);
-		initRootDir();
+		// allocate free space and root dir if existed
 	}
+	else
+	{
+		// initialize the VCB and other things if it is not initialized
+		//printf("***MAGIC NUMBER IS BAD!: %s\n", ourVCB->magicNumber);
+	}
+
+	// Test running
+	initVCB(numberOfBlocks, blockSize);
+	initFreeSpace();
+	initRootDir();
 
 	return 0;
 }
 
 void exitFileSystem()
 {
+	// TODO close all
 	printf("System exiting\n");
 }
 
 void initVCB(uint64_t numberOfBlocks, uint64_t blockSize)
 {
 	// initialize the default values of VCB for our file system
-	memcpy(ourVCB->magicNumber, MAGIC_NUMBER, strlen(MAGIC_NUMBER));
+	memcpy(ourVCB->magicNumber, MAGIC_NUMBER, MAGIC_NUMBER_LENGTH);
 	ourVCB->numberOfBlocks = numberOfBlocks;
 	ourVCB->blockSize = blockSize;
-	ourVCB->feespaceLength = ceil(numberOfBlocks / sizeof(int));
+
+	// each block needs one bit to indicate the status
+	// so we divide it by 8 to get how many bytes we need
+	// then we divide it by block size to get how many blocks we need
+	uint block = numberOfBlocks / 8 / blockSize;
+
+	// round up if needed
+	if (numberOfBlocks % blockSize > 0)
+	{
+		block++;
+	}
+	ourVCB->freespaceBlockCount = block;
+
+	/* TEST ourVCB */
+	printf("*** VCB STATUS ***\n");
+	printf("Number of Blocks: %ld\n", ourVCB->numberOfBlocks);
+	printf("Block Size: %ld\n", ourVCB->blockSize);
+	printf("Freespace Block Count: %d\n", ourVCB->freespaceBlockCount);
 
 	// prepare a clean buffer to write the data of the VCB
-	// must prepare because LBAWrite() writes a whole block
+	// must prepare a whole block because LBAWrite() writes a whole block
 	char *writeBuffer = setCleanBuffer(blockSize);
 
 	// copy the values of VCB into the buffer
-	memcpy(writeBuffer, ourVCB, sizeof(vcb_t));
+	memcpy(writeBuffer, ourVCB, sizeof(vcb));
 
-	// write the VCB into the sample volume to ensure it is physically initialized`
+	// write the VCB into the sample volume to ensure it is physically initialized
 	// TODO error handle
 	LBAwrite(writeBuffer, 1, 0);
 }
 
-void initFreeSpace(uint64_t numberOfBlocks, uint64_t blockSize)
+void initFreeSpace()
 {
-	int length = ourVCB->feespaceLength;
+	freespaceSize = ourVCB->freespaceBlockCount * ourVCB->blockSize;
 
 	// initialize the bitmap array
-	freespace = malloc(length * sizeof(int));
+	freespace = malloc(freespaceSize * sizeof(int));
 
-	// reset all bits into 0 to repesent free
-	// now the bits in each byte is 0
-	for (int i = 0; i < length; i++)
+	// reset all bytes into 0 to repesent free
+	// this is because 0 is the same as all 0 in the bit filed
+	for (int i = 0; i < freespaceSize; i++)
 	{
-		freespace[i] = 0;
+		freespace[i] = SPACE_FREE;
 	}
 
-	// mark the used block of a new volume before writing
-	int bitmapBlockCount = ceil(length / sizeof(int));
-	for (int i = 0; i < 1 + bitmapBlockCount; i++)
+	// set used block for this bitmap and VCB
+	for (int i = 0; i < ourVCB->freespaceBlockCount + 1; i++)
+	{
+		freespace[0] |= (SPACE_USED << i);
+	}
+	updateFreeSpace();
+}
+
+// changing the bit to used status
+void setBitOn(uint64_t indexOfBit)
+{
+	// TODO check bit is already 1
+	uint targetByte = indexOfBit / BIT_SIZE_OF_INT;
+	uint targetBit = indexOfBit % BIT_SIZE_OF_INT;
+	freespace[targetByte] |= (SPACE_USED << targetBit);
+}
+
+// write the new freespace array back to the volume
+void updateFreeSpace()
+{
+	char *writeBuffer = setCleanBuffer(freespaceSize);
+	memcpy(writeBuffer, freespace, freespaceSize);
+	LBAwrite(writeBuffer, ourVCB->freespaceBlockCount, 1);
+}
+
+uint64_t allocateFreeSpace(uint requestedSpace)
+{
+	// round up the exact block we need
+	uint requestedBlockCount = requestedSpace / ourVCB->blockSize;
+	if (requestedSpace % ourVCB->blockSize > 0)
+	{
+		requestedBlockCount++;
+	}
+
+	uint count = 0;
+	for (uint64_t i = 0; i < ourVCB->numberOfBlocks; i++)
 	{
 		// bitmap function: needs to be moved into a new file
-		int targetByte = floor(i / sizeof(int));
-		int targetBit = i % sizeof(int);
+		// round down to get the exact byte it is on
+		uint targetByte = i / BIT_SIZE_OF_INT;
+		uint targetBit = i % BIT_SIZE_OF_INT;
 
-		int bitNum = (int)pow(2, targetBit);
-		freespace[targetByte] |= bitNum;
+		// int result = (freespace[targetByte] & (SPACE_USED << targetBit));
+		// printf("\ncurrent: %d, result: %d", freespace[targetByte], result);
+
+		if ((freespace[targetByte] & (SPACE_USED << targetBit)) == 0)
+		{
+			count++;
+			if (count == requestedBlockCount)
+			{
+				for (; count > 0; count--)
+				{
+					setBitOn(i - count + 1);
+				}
+				updateFreeSpace();
+				return i - requestedBlockCount + 1;
+			}
+		}
+		else
+		{
+			count = 0;
+		}
 	}
 
-	// write the bitmap into the blocks
-	char *writeBuffer = setCleanBuffer(length);
-	memcpy(writeBuffer, freespace, length);
-	LBAwrite(writeBuffer, bitmapBlockCount, 1);
+	// not enough space or fragmentation
+	return -1;
 }
 
 void initRootDir()
 {
+	ourVCB->rootDirLoc = createDirectory(NULL, "root");
+
+	// write the new data of VCB
+	char *writeBuffer = setCleanBuffer(ourVCB->blockSize);
+	memcpy(writeBuffer, ourVCB, sizeof(vcb));
+	LBAwrite(writeBuffer, 1, 0);
+}
+
+uint createDirectory(dirEntry *parent, char *name)
+{
+	fdDir *newDir = malloc(sizeof(fdDir));
+	newDir->dirLoc = allocateFreeSpace(sizeof(fdDir));
+	printf("***dirLoc: %ld\n", newDir->dirLoc);
+
+	// initialize current directory entry .
+	dirEntry *currentDirEntry = malloc(sizeof(fdDir));
+	strcpy(currentDirEntry->name, name);
+	currentDirEntry->entryLoc = newDir->dirLoc;
+	currentDirEntry->type = TYPE_DIR;
+	currentDirEntry->size = 0;
+
+	// retrieve the current system time and assign it to new entries
+	time_t currentTime = time(NULL);
+	currentDirEntry->dateAccessed = currentTime;
+	currentDirEntry->dateCreated = currentTime;
+	currentDirEntry->dateModified = currentTime;
+
+	// initialize parent directory entry ..
+	dirEntry *parentDirEntry = malloc(sizeof(dirEntry));
+	if (parent == NULL)
+	{ // if parent is NULL, this is a root directory
+		memcpy(parentDirEntry, currentDirEntry, sizeof(dirEntry));
+	}
+	else
+	{ // if parent is not NULL, we copy the parent into this entry
+		memcpy(parentDirEntry, parent, sizeof(dirEntry));
+	}
+
+	// place the directory entry into the directory file
+	newDir->dirEntryAmount = 2;
+	newDir->dirEntryList[0] = *currentDirEntry;
+	newDir->dirEntryList[1] = *parentDirEntry;
+
+	// write the directory file psycially
+	LBAwrite(newDir, 5, newDir->dirLoc);
+
+	return newDir->dirLoc;
 }
--- fsLow.h
+++ fsLow.h
@@ -34,15 +34,15 @@
 // Start Partition System
 //
 // This is the first function to call before your filesystem starts
-// If the filename already exists, then the input values stored in 
-// volSize and blockSize are ignored.  If the file does not exist, it will 
+// If the filename already exists, then the input values stored in
+// volSize and blockSize are ignored.  If the file does not exist, it will
 // be created to the specified volume size in units of the block size
 // (must be power of 2) plus one for the partition header.
 //
-// On return 
+// On return
 // 		return value 0 = success;
 //		return value -1 = file exists but can not open for write
-//		return value -2 = insufficient space for the volume		
+//		return value -2 = insufficient space for the volume
 //		volSize will be filled with the volume size
 //		blockSize will be filled with the block size
 #ifndef uint64_t
@@ -53,29 +53,24 @@ typedef u_int32_t uint32_t;
 #endif
 typedef unsigned long long ull_t;
 
+int startPartitionSystem(char *filename, uint64_t *volSize, uint64_t *blockSize);
 
+int closePartitionSystem();
 
-int startPartitionSystem (char * filename, uint64_t * volSize, uint64_t * blockSize);
+int initFileSystem(uint64_t numberOfBlocks, uint64_t blockSize);
+void exitFileSystem();
 
-int closePartitionSystem ();
+uint64_t LBAwrite(void *buffer, uint64_t lbaCount, uint64_t lbaPosition);
 
-int initFileSystem (uint64_t numberOfBlocks, uint64_t blockSize);
-void exitFileSystem ();
-
-uint64_t LBAwrite (void * buffer, uint64_t lbaCount, uint64_t lbaPosition);
-
-uint64_t LBAread (void * buffer, uint64_t lbaCount, uint64_t lbaPosition);
+uint64_t LBAread(void *buffer, uint64_t lbaCount, uint64_t lbaPosition);
 
 #define MINBLOCKSIZE 512
-#define PART_SIGNATURE	0x526F626572742042
-#define PART_SIGNATURE2	0x4220747265626F52
+#define PART_SIGNATURE 0x526F626572742042
+#define PART_SIGNATURE2 0x4220747265626F52
 #define PART_CAPTION "CSC-415 - Operating Systems File System Partition Header\n\n"
 
-#define	PART_ACTIVE 		1
-#define	PART_INACTIVE 		0
+#define PART_ACTIVE 1
+#define PART_INACTIVE 0
 
-#define	PART_NOERROR 		0
-#define PART_ERR_INVALID	-4
-
-
-
+#define PART_NOERROR 0
+#define PART_ERR_INVALID -4
